4a5e9c92c7cd63fd1c44f69050898109f6fbb45e
opu183059 <opuctg440@gmail.com>
2025-04-04 22:00:01 +0600

    Books, Customers, and Orders data inserted

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,6 +1,6 @@
 -- Active: 1743780779693@@127.0.0.1@5432@bookstore_db
 
--- creating books table 
+-- Create books table 
 CREATE TABLE books (
     id SERIAL PRIMARY KEY,  
     title VARCHAR(255) NOT NULL, 
@@ -8,6 +8,23 @@ CREATE TABLE books (
     price DECIMAL(10,2) CHECK (price >= 0),  -- Price is non-negative
     stock INT CHECK (stock >= 0),  -- Stock is non-negative
     published_year INT 
+);
+
+-- Create customer table
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR(255) NOT NULL,  
+    email VARCHAR(255) UNIQUE NOT NULL,  
+    joined_date DATE DEFAULT CURRENT_DATE  
+);
+
+-- Create order table
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,  
+    customer_id INT REFERENCES customers(id) ON DELETE CASCADE,  
+    book_id INT REFERENCES books(id) ON DELETE CASCADE,  
+    quantity INT CHECK (quantity > 0),  -- quantity greater than 0
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
 );
 
 --view all table
