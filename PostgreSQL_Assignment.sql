a03e64ddb72dda1b395f9f91299a379b1f1ea94e
opu183059 <opuctg440@gmail.com>
2025-04-04 21:54:04 +0600

    created customer and order table

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,1 +1,14 @@
 -- Active: 1743780779693@@127.0.0.1@5432@bookstore_db
+
+-- creating books table 
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,  
+    title VARCHAR(255) NOT NULL, 
+    author VARCHAR(255) NOT NULL,  
+    price DECIMAL(10,2) CHECK (price >= 0),  -- Price is non-negative
+    stock INT CHECK (stock >= 0),  -- Stock is non-negative
+    published_year INT 
+);
+
+--view all table
+SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'; 
